5b4e38a4dd17823cd86bf855ed0df0bfce4e043c
ashranka <andreshranka@gmail.com>
2025-07-04 21:26:50 -0400

    Creacion tablas Pagos y Alquileres

--- AE2_ABPRO_grupo3.sql
+++ AE2_ABPRO_grupo3.sql
@@ -1,2 +1,21 @@
 CREATE DATABASE IF NOT EXISTS alquiler_automoviles;
 USE alquiler_automoviles;
+
+
+CREATE TABLE Alquileres(
+    id_alquiler INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    id_vehiculo INT NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY (id_vehiculo) REFERENCES Vehiculos(id_vehiculo)
+);
+
+CREATE TABLE Pagos(
+    id_pago INT PRIMARY KEY AUTO_INCREMENT,
+    id_alquiler INT NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    fecha_pago DATE NOT NULL,
+    FOREIGN KEY (id_alquiler) REFERENCES Alquileres(id_alquiler)
+);
